c130162f5f7dfe86f4e901dbb9a55bb0047ad2cc
DLIN131 <daiyulin945@gmail.com>
2023-10-01 17:02:30 +0800

    modify download server link

--- src/api/downloadData.js
+++ src/api/downloadData.js
@@ -1,5 +1,5 @@
 export const downloadData = async (videoId) => {
   // return response =  request.get(`https://ytdl-server-byvu.onrender.com/${videoId}`)
-  return await fetch(`https://ytdl-server-byvu.onrender.com/${videoId}`)
+  return await fetch(`https://ytdl-server-byvu.onrender.com/download/${videoId}`)
   // return await fetch(`http://localhost:5000/download/${videoId}`)
 }
